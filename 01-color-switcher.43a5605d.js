4db080f39f13681ba6a80b2d3d1ffe3899c14b8e
Cristina-Irina <121606106+Cristina-Irina@users.noreply.github.com>
2023-06-24 19:06:13 +0000

    Deploying to gh-pages from @ Cristina-Irina/goit-js-hw-09@cf19e7921257f274b016e0e43886be759a1fcf61 🚀

---
+++ 01-color-switcher.43a5605d.js
@@ -0,0 +1,2 @@
+var t=null,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",function(){null===t&&(t=setInterval(function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`},1e3),e.disabled=!0,a.disabled=!1)}),a.addEventListener("click",function(){clearInterval(t),t=null,e.disabled=!1,a.disabled=!0});
+//# sourceMappingURL=01-color-switcher.43a5605d.js.map
